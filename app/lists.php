4a2d1f169f715413818e8dba5bcaa36326257a22
Saeid Mohadjer <saeid@fastmail.fm>
2018-12-15 23:30:50 +0100

    adds lists page

---
+++ app/lists.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Saeid Mohadjer</title>
+		<meta name="description" content="Frontend Web Developer (freelance) in Freiburg, Germany" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="format-detection" content="telephone=no" />
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+		<?php include('include/styles.html'); ?>
+	</head>
+	<body>
+		<?php include('include/header.html'); ?>
+		<?php include('content/lists.html'); ?>
+		<?php include('include/footer.html'); ?>
+	</body>
+</html>
