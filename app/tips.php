4b456f234174c503649ff4b4104703edc7c7f6c2
Saeid Mohadjer <saeid@fastmail.fm>
2020-08-30 20:37:06 +0200

    removes gulp

--- app/tips.php
+++ app/tips.php
@@ -7,11 +7,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="format-detection" content="telephone=no" />
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-		<?php include('include/styles.html'); ?>
-	</head>
+		<!-- build:css({.tmp,app}) resources/css/styles.min.css -->
+		<link rel="stylesheet" href="../.tmp/resources/css/helper.css">
+		<link rel="stylesheet" href="../.tmp/resources/css/grid.css" />
+		<link rel="stylesheet" href="../.tmp/resources/css/styles.css">
+		<!-- endbuild -->
+		</head>
 	<body>
-		<?php include('include/header.html'); ?>
+		<?php include('includes/header.html'); ?>
 		<?php include('content/tips.html'); ?>
-		<?php include('include/footer.html'); ?>
+		<?php include('includes/footer.html'); ?>
 	</body>
 </html>
--- gulp-tasks/watch.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', function() {
-	gulp.watch('app/resources/css/**/*.scss', ['sass']);
-	gulp.watch('app/resources/hbs/*.hbs', ['hbs']);
-	gulp.watch('app/resources/js/*.js', ['jsHint', 'transpile']);
-	gulp.watch('.tmp/resources/css/*.css', ['cssLint']);
-});
