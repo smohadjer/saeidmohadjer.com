61c3dc463f5994bd9a8b3791cb80ce0b38b69433
smohadjer <saeid@fastmail.fm>
2022-11-23 12:32:27 +0000

    Deploying to gh-pages from @ smohadjer/saeidmohadjer.com@c2fe9d0eb9327dd9ee44c6040d7f8385b4d932b6 🚀

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,11 +2,13 @@
 	'use strict';
 
 	//import Navigation from './modules/nav.js';
+
 	//Helper classes to HTML for styling of nojs version
 	var html = document.querySelector('html');
 	html.classList.remove('no-js');
-	html.classList.add('js'); //taken from http://youmightnotneedjquery.com/
+	html.classList.add('js');
 
+	//taken from http://youmightnotneedjquery.com/
 	function ready(fn) {
 
 	  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
@@ -15,7 +17,6 @@
 	    document.addEventListener('DOMContentLoaded', fn);
 	  }
 	}
-
 	ready(function () {
 
 	  console.log('DOM is ready!');
