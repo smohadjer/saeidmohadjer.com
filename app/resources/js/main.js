1497da1cc706a5de4642dbc9ce1800239ae5267a
Saeid Mohadjer <saeid@fastmail.fm>
2022-11-25 21:38:23 +0100

    adds swiper

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -1,4 +1,6 @@
 //import Navigation from './modules/nav.js';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+
 
 //Helper classes to HTML for styling of nojs version
 const html = document.querySelector('html');
@@ -20,5 +22,19 @@ ready(function() {
 	'use strict';
 
 	console.log('DOM is ready!');
+
+	const swiper = new Swiper('.swiper', {
+		// Optional parameters
+		loop: true,
+	
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+	
+		// Navigation arrows
+		navigation: false
+	});
 });
 
