d0e79088209a11ba08358faf1bf2ed4e73edc478
Saeid Mohadjer <saeid@fastmail.fm>
2024-08-25 19:51:19 +0200

    adds sorting to tags

--- api/_getTags.ts
+++ api/_getTags.ts
@@ -1,21 +1,39 @@
-type Item = {
-    tags: string[]
+type Document = {
+    tags?: string[]
 };
+type Tag = {
+    label: string;
+    count: number;
+}
+
+const allTags: Tag[] = [];
+
+const addTag = (tag: string) => {
+    const foundItem = allTags.find(item => item.label === tag);
+    if (foundItem) {
+        foundItem.count += 1;
+    } else {
+        allTags.push({
+            label: tag,
+            count: 1
+        });
+    }
+}
 
 export default async (collection) => {
     const docs = await collection.find({
         permission: { $ne: 'private' }
     }).toArray();
-    const allTags: string[] = [];
-    docs.forEach((item: Item) => {
-        if (item.tags) {
-            item.tags.forEach(tag => {
-                if (!allTags.includes(tag)) {
-                    allTags.push(tag);
-                }
+
+    docs.forEach((doc: Document) => {
+        if (doc.tags) {
+            doc.tags.forEach((tag) => {
+                addTag(tag);
             });
         }
     });
 
+    // sort tags based on how often they are used
+    allTags.sort((a, b) =>  b.count - a.count);
     return allTags;
 }
